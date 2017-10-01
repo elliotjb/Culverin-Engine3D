191c608d86b36b93f7f9a18f9727841c70818a88
Jordior97 <jordior97@gmail.com>
2017-10-01 19:48:02 +0200

    commit fix

--- Engine/ModulePhysics3D.cpp
+++ Engine/ModulePhysics3D.cpp
@@ -952,7 +952,9 @@ void ModulePhysics3D::ShowPerformance(int ms_index)
 		ImGui::PlotHistogram("", pre_log, IM_ARRAYSIZE(pre_log), 0, NULL, 0.0f, 5.0f, ImVec2(0, 80));
 		ImGui::Text("Update:"); ImGui::SameLine();
 		ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "%.4f", up_log[ms_index - 1]);
+		ImGui::PlotHistogram("", up_log, IM_ARRAYSIZE(up_log), 0, NULL, 0.0f, 5.0f, ImVec2(0, 80));
 		ImGui::Text("Post-Update:"); ImGui::SameLine();
 		ImGui::TextColored(ImVec4(1.0f, 1.0f, 0.0f, 1.0f), "%.4f", post_log[ms_index - 1]);
+		ImGui::PlotHistogram("", post_log, IM_ARRAYSIZE(post_log), 0, NULL, 0.0f, 5.0f, ImVec2(0, 80));
 	}
 }
