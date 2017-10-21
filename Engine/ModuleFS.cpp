5ed596686f1913e138e1e72cddbab8b59ccada2b
elliotjb <elliot.jimenez1@gmail.com>
2017-10-21 15:17:54 +0200

    Add SaveFile

--- Engine/ModuleFS.cpp
+++ Engine/ModuleFS.cpp
@@ -1,9 +1,5 @@
 #include "ModuleFS.h"
-#include <filesystem>
-#include <iostream>
-#include <experimental/filesystem>
-#include <fstream>
-
+#include "Application.h"
 
 ModuleFS::ModuleFS(bool start_enabled) : Module(start_enabled)
 {
@@ -16,12 +12,48 @@ ModuleFS::~ModuleFS()
 
 bool ModuleFS::Init(JSON_Object * node)
 {
+
 	return true;
 }
 
 bool ModuleFS::Start()
 {
+	// Will contain exe path
+	HMODULE hModule = GetModuleHandle(NULL);
+	if (hModule != NULL)
+	{
+		// When passing NULL to GetModuleHandle, it returns handle of exe itself
+		GetModuleFileName(hModule, ownPth, (sizeof(ownPth)));
+	}
 	return true;
+}
+
+update_status ModuleFS::PreUpdate(float dt)
+{
+	if (App->input->GetKey(SDL_SCANCODE_N) == KEY_DOWN)
+	{
+		files = Get_filenames(ownPth);
+	}
+
+
+	return UPDATE_CONTINUE;
+}
+
+std::vector<std::string> ModuleFS::Get_filenames(std::experimental::filesystem::path path)
+{
+	namespace stdfs = std::experimental::filesystem;
+
+	std::vector<std::string> filenames;
+
+	const stdfs::directory_iterator end{};
+
+	for (stdfs::directory_iterator iter{ path }; iter != end; ++iter)
+	{
+		if (stdfs::is_regular_file(*iter)) // comment out if all names (names of directories tc.) are required
+			filenames.push_back(iter->path().string());
+	}
+
+	return filenames;
 }
 
 uint ModuleFS::LoadFile(const char* file, char** buffer)
@@ -55,6 +87,27 @@ uint ModuleFS::LoadFile(const char* file, char** buffer)
 		LOG("Error to Load File -> %s", file);
 	}
 	return length;
+}
+
+bool ModuleFS::SaveFile(const char* data, std::string name, uint size)
+{
+	//Move to FileSystem ----------------------------------------
+	std::ofstream outfile(name, std::ofstream::binary);
+
+	if (outfile.good())
+	{
+		// write to outfile
+		outfile.write(data, size);
+		LOG("Save Completed");
+	}
+	else
+	{
+		LOG("Failed to write the file %s", name.c_str());
+	}
+
+	outfile.close();
+	// ------------------------------------------------------------
+	return false;
 }
 
 
--- Engine/ModuleFS.h
+++ Engine/ModuleFS.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "Globals.h"
 #include "Module.h"
+#include <filesystem>
+#include <iostream>
+#include <experimental/filesystem>
+#include <fstream>
 
 class ModuleFS : public Module
 {
@@ -11,11 +15,17 @@ public:
 	bool Init(JSON_Object* node);
 	bool Start();
 
-	//update_status PreUpdate(float dt);
+	update_status PreUpdate(float dt);
+	std::vector<std::string> Get_filenames(std::experimental::filesystem::path path);
 	//update_status Update(float dt);
 	//update_status PostUpdate(float dt);
 	//update_status UpdateConfig(float dt);
 
 	uint LoadFile(const char* file, char** buffer);
+	bool SaveFile(const char* data, std::string name, uint size);
+
+public:
+	std::vector<std::string> files;
+	char ownPth[MAX_PATH];
 };
 
