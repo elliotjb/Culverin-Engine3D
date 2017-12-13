ccffa2f420bc9153a12042c6960718baebf8c009
elliotjb <elliot.jimenez1@gmail.com>
2017-12-13 12:12:02 +0100

    Add Reference CulverinEditor

--- CulverinEditor/CulverinEditor/Console.cs
+++ CulverinEditor/CulverinEditor/Console.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System.Configuration.Assemblies;
+
+namespace CulverinEditor.Console
+{
+    public class Console
+    {
+        public string Log(string var)
+        {
+            return var;
+        }
+        public string Log(int var)
+        {
+            return var.ToString();
+        }
+        public string Log(float var)
+        {
+            return var.ToString();
+        }
+    }
+}
---
+++ TestUsing/TestUsing/Test.cs
@@ -0,0 +1,17 @@
+﻿using CulverinEditor;
+using CulverinEditor.Console;
+
+public class Test : Culverin
+{
+    void Testd()
+    {
+        Getvar();
+    }
+    
+    void Update()
+    {
+        int temp = Getvar();
+        
+    }
+    
+}
